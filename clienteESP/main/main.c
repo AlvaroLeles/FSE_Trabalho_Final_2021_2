827d2129cd67773e7e53b00e77c4a184841a0e84
AlineLermen <aline.helena.lermen@gmail.com>
2022-05-02 20:26:17 -0300

    Adiciona conexão wifi

---
+++ clienteESP/main/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "freertos/semphr.h"
+
+#include "wifi.h"
+
+SemaphoreHandle_t conexaoWifiSemaphore;
+
+void app_main(void)
+{
+    // Inicializa o NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    conexaoWifiSemaphore = xSemaphoreCreateBinary();
+    wifi_start();
+    
+}
